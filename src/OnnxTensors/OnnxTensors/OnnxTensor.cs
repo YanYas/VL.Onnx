b53c697aecac29473f7fa1bdef8ef2030b410da8
yanyas <hayden.anyasi@network.rca.ac.uk>
2020-01-09 16:47:41 +0000

    Create wrapper so VL-roslyn can avoid ReadOnlySpan<int> which throws errors

---
+++ src/OnnxTensors/OnnxTensors/OnnxTensor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Numerics.Tensors;
+
+
+namespace VL.ONNX
+{
+    public static class OnnxTensors<T>
+    {
+        public static Tensor<T> CreateNewTensor(Memory<T> InputData, int[] Dimensions)
+        {
+            var DimSpan = new ReadOnlySpan<int>(Dimensions);
+            return new DenseTensor<T>(InputData, DimSpan);
+        }
+    }
+}
